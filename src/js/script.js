80dd3a3c1d3447264d6e8322a1c5578b9a6e1ae8
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-04-21 21:45:45 +0200

    feat: added helper functions updateUI and resetGame to help reset and redraw the board UI from a saved set or a new game

--- src/js/script.js
+++ src/js/script.js
@@ -16,6 +16,24 @@ const winningCombinations = [
 ];
 
 const cells = Array.from(document.querySelectorAll(".cell"));
+const modal = document.getElementById("gameEndModal");
+const modalTitle = document.querySelector("#gameEndModal h1");
+const restartButton = document.querySelector("#gameEndModal button");
+const appDiv = document.getElementById("app");
+
+function updateUI() {
+  cells.forEach((cell, index) => {
+    cell.textContent = gameState.board[index];
+  });
+}
+
+function resetGame() {
+    gameState.board.fill("");
+    gameState.currentPlayer = "X";
+    gameState.gameActive = true;
+    updateUI();
+    modal.classList.add("hidden");
+}
 
 cells.forEach((cell) => {
   cell.addEventListener("click", () => cellClick(cell));
