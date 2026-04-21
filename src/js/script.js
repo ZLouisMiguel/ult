7941f8e110d4314497bc0c3784287b262a5a68ca
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-04-21 20:49:16 +0200

    feat: add global game state object and cellClick function to handle click in the board cells

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,15 @@
+const gameState = {
+  currentPlayer: "X",
+};
+
 const cells = Array.from(document.querySelectorAll(".cell"));
 
-console.log(cells);
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => cellClick(cell));
+});
+
+const cellClick = (cell) => {
+  if (cell.textContent == "X" || cell.textContent == "O") return;
+  cell.textContent = gameState.currentPlayer;
+  gameState.currentPlayer = gameState.currentPlayer == "X" ? "O" : "X";
+};
