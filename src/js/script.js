47a8ca072ebcc1c51c2d80d5d12a253bd5b9a3f5
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-04-21 21:39:33 +0200

    feat: add winning combinations array for easy matching and checking of winning conditions

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,17 @@ const gameState = {
   gameActive: true,
 };
 
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const cells = Array.from(document.querySelectorAll(".cell"));
 
 cells.forEach((cell) => {
